931a0fccdc0ff8a495e26b004830eaebff3d73f3
OmMandlik02 <89374137+OmMandlik02@users.noreply.github.com>
2022-09-01 17:33:45 +0530

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,6 @@
+const element = document.getElementById("btn")
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  alert("Your contact request has been send suceesfully . We will contact you within 2 working days");
+}
